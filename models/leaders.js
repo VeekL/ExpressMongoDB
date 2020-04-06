d1710dbede4e4f3469162a98f3ea82293ad0927a
VeekL <veeklee92@gmail.com>
2020-04-06 15:30:19 +0800

    Express Session Part 2

--- models/leaders.js
+++ models/leaders.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-require('mongoose-currency').loadType(mongoose);
-const Currency = mongoose.Types.Currency;
 
 const leaderSchema = new Schema({
     name: {
