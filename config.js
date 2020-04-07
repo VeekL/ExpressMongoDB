f8f573d7d6fa38f33d193cd2554448b18a132320
VeekL <veeklee92@gmail.com>
2020-04-07 22:13:33 +0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2324550014512279',
+        clientSecret: '82d0d160c4ebd6f45d42d6749445bbf1'
+    }
 }
